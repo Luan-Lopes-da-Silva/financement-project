682e1f52de6a007d7a59988b558867a1fca8a3e6
Luan-Lopes-da-Silva <lopesluan18@outlook.com>
2023-09-12 18:05:54 -0300

    Some rules in form and stylish

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,4 @@
+import Script from 'next/script'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -10,7 +11,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}
+      <Script src='https://cdn.lordicon.com/bhenfmcm.js'/>
+      </body>
+      
     </html>
   )
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,10 @@
 'use client'
 import { useRef, useState } from 'react'
-import style from './page.module.css'
 import html2canvas from 'html2canvas'
 import jsPDF from 'jspdf'
+import style from './page.module.scss'
+import pdfSvg from './src/assets/pdf-svgrepo-com.svg'
+import Image from 'next/image'
 
 export default function Home() {
   const simulations = []
@@ -11,109 +13,195 @@ export default function Home() {
   const [entrada,setEntrada] = useState('')
   const [prazo,setPrazo] = useState('')
   const [juros,setJuros] = useState('6%')
-  const [simul , setSimul] = useState([])
   const refParcela = useRef(null)
   const refJuros = useRef(null)
   const refAmortizacao = useRef(null)
   const refValorParcela = useRef(null)
   const refSaldo = useRef(null)
   const targetRef = useRef()
+  const mensage = useRef()
+  const mensagePorcentagemFinanciamento = useRef()
+  const mensageEntrada = useRef()
+  const mensageParcela = useRef()
+  const refTable = useRef()
+
+  function minValues(ev){
+    if(ev.currentTarget.value < 40000){
+      mensage.current.innerText = 'Valor minimo de imovel R$ 40,000'
+    }else{
+      mensage.current.innerText = ''
+      mensagePorcentagemFinanciamento.current.innerText = ''
+      mensageEntrada.current.innerText = ''
+      const financiamento = (ev.currentTarget.value * 80) / 100
+      setFinanciamento(financiamento)
+      setEntrada(imovel-financiamento)
+    }
+  }
+
+  function maxValue(ev){
+    const porcentagem = (ev.currentTarget.value/imovel) * 100 
+    if(porcentagem>80){
+    mensagePorcentagemFinanciamento.current.innerText = 'Porcentagem máxima de financiamento atingida abaixe o valor'
+    }else{
+    mensagePorcentagemFinanciamento.current.innerText = ''
+    setEntrada(imovel-financiamento) 
+    }
+  }
 
   function createSimulation(ev){
     ev.preventDefault()
-    const values = []
-    values.push(Number(financiamento))
-  for(let i = 1; i<Number(prazo); i++){
-    values.push((Number(imovel) - Number(entrada)) / prazo)
-      var simulation = {
-      parcelas:`Parcela ${i}`,
-      valorParcela: (Number(imovel) - Number(entrada)) / prazo,
-      juros: '0,48',
-      financiado: financiamento,
-      amortizacao: Number(financiamento) / Number(prazo),
-      }
+    const porcentagem = (financiamento/imovel) * 100 
+    if(financiamento === '' && imovel === ''){
+      mensage.current.innerText = 'Preencha o valor do imovel'
+      mensagePorcentagemFinanciamento.current.innerText = 'Preencha o valor do financiamento'
+      mensageParcela.current.innerText = 'Preencha o numero de parcelas'
+      mensageEntrada.current.innerText = 'Preencha o valor de entrada'
+    }else if(imovel === ''){
+      mensage.current.innerText = 'Preencha o valor do imovel'
+    }else if(financiamento === ''){
+      mensagePorcentagemFinanciamento.current.innerText = 'Preencha o valor do financiamento'
+    }else if(prazo === ''){
+      mensageParcela.current.innerText = 'Preencha o numero de parcelas'
+    }else if(entrada === '') {
+      mensageEntrada.current.innerText = 'Preencha o valor de entrada'
+    }
+      else if(porcentagem>80){
+      mensagePorcentagemFinanciamento.current.innerText = 'Porcentagem máxima de financiamento atingida abaixe o valor'
+    }else{
+      refTable.current.style.display = 'block'
+      mensagePorcentagemFinanciamento.current.innerText = ''
+      mensageParcela.current.innerText = ''
+      mensageEntrada.current.innerText = ''
+      const values = []
+      values.push(Number(financiamento))
+      const parcela0Span = document.createElement('p')
+      parcela0Span.innerText = 'Parcela 0'
+      refParcela.current.appendChild(parcela0Span)
+
+      const parcela0Juros = document.createElement('p')
+      parcela0Juros.innerText = '-'
+      refJuros.current.appendChild(parcela0Juros)
+      
+      const parcela0Amortizacao = document.createElement('p')
+      parcela0Amortizacao.innerText = '-'
+      refAmortizacao.current.appendChild(parcela0Amortizacao)
+
+      const parcela0Valor = document.createElement('p')
+      parcela0Valor.innerText = '-'
+      refValorParcela.current.appendChild(parcela0Valor)
+
+      const parcela0Saldo = document.createElement('p')
+      parcela0Saldo.innerText = 'R$ 000'
+      refSaldo.current.appendChild(parcela0Saldo)
+
+      for(let i = 1; i<=Number(prazo); i++){
+        values.push((Number(imovel) - Number(entrada)) / prazo)
+        var simulation = {
+        parcelas:`Parcela ${i}`,
+        valorParcela: (Number(imovel) - Number(entrada)) / prazo,
+        juros: '0,48',
+        financiado: financiamento,
+        amortizacao: Number(financiamento) / Number(prazo),
+        }
+    
+        const spanParcela = document.createElement('p')
+        spanParcela.innerText = simulation.parcelas
+        refParcela.current.appendChild(spanParcela)
   
-      const spanParcela = document.createElement('p')
-      spanParcela.innerText = simulation.parcelas
-      refParcela.current.appendChild(spanParcela)
-
-      const spanJuros = document.createElement('p')
-      spanJuros.innerText = simulation.juros
-      refJuros.current.appendChild(spanJuros)
-      
-      const spanAmortizacao = document.createElement('p')
-      spanAmortizacao.innerText = simulation.amortizacao
-      refAmortizacao.current.appendChild(spanAmortizacao)
-
-      const spanValorParcela = document.createElement('p')
-      spanValorParcela.innerText = simulation.valorParcela
-      refValorParcela.current.appendChild(spanValorParcela)
-
-      
-      
-
-  simulations.push(simulation)
-  setSimul(state => [...state,simulation])
-  }
-  const result = values.reduce((acc,cur)=>{
-    const spanSaldo = document.createElement('p')
-    spanSaldo.innerText = `R$ ${acc} - R$ ${cur} = R$ ${acc-cur}`
-    refSaldo.current.appendChild(spanSaldo)
-    return acc-cur
-  })
+        const spanJuros = document.createElement('p')
+        spanJuros.innerText = simulation.juros
+        refJuros.current.appendChild(spanJuros)
+        
+        const spanAmortizacao = document.createElement('p')
+        spanAmortizacao.innerText = simulation.amortizacao
+        refAmortizacao.current.appendChild(spanAmortizacao)
+  
+        const spanValorParcela = document.createElement('p')
+        spanValorParcela.innerText = simulation.valorParcela
+        refValorParcela.current.appendChild(spanValorParcela)  
+        simulations.push(simulation)
+    }
+        const result = values.reduce((acc,cur)=>{
+        const spanSaldo = document.createElement('p')
+        spanSaldo.innerText = `R$ ${acc} - R$ ${cur} = R$ ${acc-cur}`
+        refSaldo.current.appendChild(spanSaldo)
+        return acc-cur
+        })
+    }   
   }
 
   const downloadPDF = () =>{
-    const table = targetRef.current
-    console.log(table)
-    html2canvas(table,{logging:true, letterRendering:1 , useCORS: true}).then(canvas=>{
-     const imgWidth = 208
-     const imgHeight= canvas.height * imgWidth / canvas.width 
-     const imgData = canvas.toDataURL('img/png')
-     const pdf = new jsPDF('p','mm','a4')
-     pdf.addImage(imgData,'PNG',0,0,imgWidth,imgHeight)
-     pdf.save('simulation.pdf')
-    })
+      const table = targetRef.current
+      html2canvas(table,{logging:true, letterRendering:1 , useCORS: true}).then(canvas=>{
+       const imgWidth = 208
+       const imgHeight= canvas.height * imgWidth / canvas.width 
+       const imgData = canvas.toDataURL('img/png')
+       const pdf = new jsPDF('p','mm','a4')
+       pdf.addImage(imgData,'PNG',0,0,imgWidth,imgHeight)
+       pdf.save('simulation.pdf')
+      })
+    
   }
   
   return (
-    <>
-    <main>
+    <div className={style.container}>
+    <header>
+      <h1>Logo</h1>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Services</li>
+          <li>About us</li>
+        </ul>
+      </nav>
+    </header>
+    <main className={style.main}>
       <form 
       className={style.form}
       onSubmit={(ev)=>createSimulation(ev)}
       >
-        <label htmlFor="valorImovel">Valor do imovel</label>
-        <input 
-        type="text" 
-        name="valorImovel"
-        value={imovel}
-        onChange={(ev)=>setImovel(ev.currentTarget.value)}
-        />
-        
-        <label htmlFor="valorEntrada">Valor da entrada</label>
-        <input 
-        type="text" 
-        name="valorEntrada"
-        value={entrada}
-        onChange={(ev)=>setEntrada(ev.currentTarget.value)}
-        onKeyUp={(ev)=>setFinanciamento(Number(imovel) - Number(ev.currentTarget.value))}
-        />
-
-        <label htmlFor="valorFinanciamento">Valor do financiamento</label>
-        <input 
-        type="text" 
-        name="valorFinanciamento" 
-        value={financiamento}
-        onChange={(ev)=>setFinanciamento(ev.currentTarget.value)}
-        />
-        <label htmlFor="prazoFinanciamento">Prazo financiamento</label>
-        <input 
-        type="text" 
-        name="prazoFinanciamento" 
-        value={prazo}
-        onChange={(ev)=>setPrazo(ev.currentTarget.value)}
-        />
+        <div>
+          <label htmlFor="valorImovel">Valor do imovel</label>
+          <span className={style.errorSpan} ref={mensage}></span>
+          <input
+          type="text"
+          name="valorImovel"
+          value={imovel}
+          onChange={(ev)=>setImovel(ev.currentTarget.value)}
+          onKeyUp={(ev)=>minValues(ev)}
+          />
+        </div>
+        <div>
+          <label htmlFor="valorFinanciamento">Valor do financiamento</label>
+          <span className={style.errorSpan} ref={mensagePorcentagemFinanciamento}></span>
+          <input
+          type="text"
+          name="valorFinanciamento"
+          value={financiamento}
+          onChange={(ev)=>setFinanciamento(ev.currentTarget.value)}
+          onKeyUp={(ev)=>maxValue(ev)}
+          />
+        </div>
+        <div>
+          <label htmlFor="valorEntrada">Valor da entrada</label>
+          <span className={style.errorSpan} ref={mensageEntrada}></span>
+          <input
+          type="text"
+          name="valorEntrada"
+          value={entrada}
+          onChange={(ev)=>setEntrada(ev.currentTarget.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="prazoFinanciamento">Prazo financiamento</label>
+          <span className={style.errorSpan} ref={mensageParcela}></span>
+          <input
+          type="text"
+          name="prazoFinanciamento"
+          value={prazo}
+          onChange={(ev)=>setPrazo(ev.currentTarget.value)}
+          />
+        </div>
         <label htmlFor="juros">Taxa juros</label>
         <input 
         type="text" 
@@ -124,25 +212,39 @@ export default function Home() {
         <button>Simular</button>
        
       </form>
-    <table ref={targetRef}>
-  <tr>
-    <th style={{color:'red'}}>Parcelas</th>
-    <th style={{color:'red'}}>Juros</th>
-    <th style={{color:'red'}}>Amortização</th>
-    <th style={{color:'red'}}>Valor Parcela</th>
-    <th style={{color:'red'}}>Saldo devedor</th>
-  </tr>
-  <tr>
-    <td style={{color:'red'}} ref={refParcela}></td>
-    <td style={{color:'red'}} ref={refJuros}></td>
-    <td style={{color:'red'}} ref={refAmortizacao}></td>
-    <td style={{color:'red'}} ref={refValorParcela}></td>
-    <td style={{color:'red'}} ref={refSaldo}></td>
-  </tr>
-
-</table>
-<button onClick={downloadPDF}>Gerar PDF</button> 
+  <div 
+  className={style.tableContainer}
+  ref={refTable}
+  >
+    <table ref={targetRef} className={style.table}>
+        <thead>
+          <tr className={style.header}>
+            <th>Parcelas</th>
+            <th>Juros</th>
+            <th>Amortizacao</th>
+            <th>Valor Parcela</th>
+            <th>Saldo devedor</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className={style.content}>
+            <td ref={refParcela}></td>
+            <td ref={refJuros}></td>
+            <td ref={refAmortizacao}></td>
+            <td ref={refValorParcela}></td>
+            <td ref={refSaldo}></td>
+          </tr>
+        </tbody>
+    </table>
+    <button button onClick={downloadPDF}>
+    <Image
+    alt='pdfBtn'
+    src={pdfSvg}
+    width={30}
+    />
+    </button> 
+  </div>
     </main>
-    </>
+    </div>
   )
 }
