d6161709d8339565115738122d28e774941d94d8
Luan-Lopes-da-Silva <lopesluan18@outlook.com>
2023-09-14 18:09:27 -0300

    Fixing some errors and make new functions

--- app/page.jsx
+++ app/page.jsx
@@ -9,13 +9,13 @@ import Image from 'next/image'
 
 export default function Home() {
   const simulations = []
-  const [imovel,setImovel] = useState(0)
-  const [aniversario,setAniversario] = useState()
-  const [banco,setBanco] = useState('')
-  const [financiamento,setFinanciamento] = useState(0)
-  const [entrada,setEntrada] = useState(0)
-  const [prazo,setPrazo] = useState(0)
-  const [juros,setJuros] = useState('6%')
+  const [imovel,setImovel] = useState('')
+  const [aniversario,setAniversario] = useState('')
+  const [banco,setBanco] = useState('Selecione um banco')
+  const [financiamento,setFinanciamento] = useState('')
+  const [entrada,setEntrada] = useState('')
+  const [prazo,setPrazo] = useState('')
+  const [juros,setJuros] = useState('')
   const refParcela = useRef(null)
   const refJuros = useRef(null)
   const refAmortizacao = useRef(null)
@@ -26,23 +26,50 @@ export default function Home() {
   const mensagePorcentagemFinanciamento = useRef()
   const mensageEntrada = useRef()
   const mensageParcela = useRef()
+  const mensageBanco = useRef()
+  const mensageJuros = useRef()
   const refTable = useRef()
   const btnRef = useRef()
-  let click = 0
- function checkField(ev){
+  const btnAtualizar = useRef()
+  const btnLimpar = useRef()
+  const mensageAniversario = useRef()
+
+  function checkIdade(ev){
+    
+    const nascimento = ev.currentTarget.value
+    const nascimentoConvertido = new Date(nascimento).getFullYear()
+    const anoAtual = new Date().getFullYear()
+    setAniversario(ev.currentTarget.value)
+    if(anoAtual-nascimentoConvertido>80){
+      mensageAniversario.current.innerText = 'Idade acima do permitido para financiamento.'
+    }else if(anoAtual-nascimentoConvertido<18){
+      mensageAniversario.current.innerText = 'Idade abaixo do permitido para financiamento.'
+    }else{
+      mensageAniversario.current.innerText = ''
+    }
+  }
+ 
+  function checkField(ev){
   const div =  ev.currentTarget.parentElement
   const span = div.querySelector('span')
+  const valorEntrada = (imovel*20) /100
   if(ev.currentTarget.value === '' || ev.currentTarget.value === '0'){
     span.innerText = 'Preencha esse campo corretamente'
+  }else if(ev.currentTarget.name === 'valorEntrada'  && ev.currentTarget.value<valorEntrada){
+    span.innerText = `Valor minimo de entrada R$ ${valorEntrada},00`
   }else{
     span.innerText = ''
-
   }
  }
+
+
 function maxPrazos(ev){
   const target = ev.currentTarget
   setPrazo(target.value)
-  if(banco === 'bradesco' && ev.currentTarget.value>420){
+  if(Number(ev.currentTarget.value)<12){
+    mensageParcela.current.innerText = 'Numero minimo de parcelas 12'
+  }
+  else if(banco === 'bradesco' && ev.currentTarget.value>420){
    mensageParcela.current.innerText = 'Limite de parcelas para esse banco ja foi atingida'
   }else{
    mensageParcela.current.innerText = ''
@@ -50,6 +77,7 @@ function maxPrazos(ev){
 }
 
   function minValues(ev){
+    const valorEntrada = (ev.currentTarget.value*20) /100
     if(ev.currentTarget.value < 40000){
       mensage.current.innerText = 'Valor minimo de imovel R$ 40,000'
     }else{
@@ -59,49 +87,173 @@ function maxPrazos(ev){
       mensageParcela.current.innerText = ''
       const financiamento = (ev.currentTarget.value * 80) / 100
       setFinanciamento(financiamento)
-      setEntrada(imovel-financiamento)
-      setPrazo(12)
+      setEntrada(valorEntrada)
     }
   }
 
   function maxValue(ev){
-    const valorEntrada = (ev.currentTarget.value*20) /100
     const porcentagem = (ev.currentTarget.value/imovel) * 100 
     if(porcentagem>80){
     mensagePorcentagemFinanciamento.current.innerText = 'Porcentagem máxima de financiamento atingida abaixe o valor'
     }else{
     mensagePorcentagemFinanciamento.current.innerText = ''
-    setEntrada(valorEntrada) 
     }
   }
 
   function bancoPrazo(ev){
   const target = ev.currentTarget
   setBanco(target.value)
-  if(target.value === 'bradesco'){
+  if(banco !='Selecione um banco'){
+  mensageJuros.current.innerText = ''
+  mensageBanco.current.innerText = ''
+  }
+  else if(target.value === 'bradesco'){
     setPrazo(420)
+    setJuros('6%')
     mensageParcela.current.innerText = ''
+    mensageJuros.current.innerText = ''
+    mensageBanco.current.innerText = ''
   }else{
+    mensageBanco.current.innerText = 'Selecione um banco valido para dar continuidade'
     console.log('Outro banco')
   }
   }
 
   function clearTable(){
-  const jurosChild = refJuros.current.children
-  const parcelasChild = refParcela.current.children
-  const amortizacaoChild = refAmortizacao.current.children
-  const valorParcelaChild = refValorParcela.current.children
-  const saldoChild = refSaldo.current.children
+    const valorEntrada = (imovel*20) /100
+    const porcentagem = (financiamento/imovel) * 100 
+    const nascimento = new Date(aniversario).getFullYear()
+    const anoAtual = new Date().getFullYear()
 
-  for (let index = 0; index < jurosChild.length; index++) {
-    jurosChild[index].innerHTML = ""
-    parcelasChild[index].innerHTML = ""
-    amortizacaoChild[index].innerHTML = ""
-    valorParcelaChild[index].innerHTML = ""
-    saldoChild[index].innerHTML = ""
+    if(banco === 'Selecione um banco'){
+      mensageBanco.current.innerText = 'Selecione um banco valido para dar continuidade'
+      mensageJuros.current.innerText = 'Preencha a taxa de juros'
+    } 
+    else if(aniversario === ''){
+      mensageAniversario.current.innerText = 'Preencha com sua data de nascimento para dar continuidade.'
+    }else if(anoAtual-nascimento>80){
+      mensageAniversario.current.innerText = 'Idade acima do permitido para financiamento.'
+    }else if(anoAtual-nascimento<18){
+      mensageAniversario.current.innerText = 'Idade abaixo do permitido para financiamento.'
+    }
+    else if(financiamento === '' && imovel === ''){
+    mensage.current.innerText = 'Preencha o valor do imovel'
+    mensagePorcentagemFinanciamento.current.innerText = 'Preencha o valor do financiamento'
+    mensageEntrada.current.innerText = 'Preencha o valor de entrada'
+    mensageBanco.current.innerText = ''
+    mensageJuros.current.innerText = ''
+    mensageAniversario.current.innerText = ''
+  }else if(financiamento === 0 && imovel === 0){
+    mensageEntrada.current.innerText = 'Preencha o valor de entrada'
+    mensage.current.innerText = 'Preencha o valor do imovel'
+    mensagePorcentagemFinanciamento.current.innerText = 'Preencha o valor do financiamento'
+    mensageEntrada.current.innerText = 'Preencha o valor de entrada'
+    mensageBanco.current.innerText = ''
+    mensageJuros.current.innerText = ''
+    mensageAniversario.current.innerText = ''
+  }else if(imovel === 0){
+    mensage.current.innerText = 'Preencha o valor do imovel'
+  }else if(financiamento === 0){
+    mensagePorcentagemFinanciamento.current.innerText = 'Preencha o valor do financiamento'
+  }else if(prazo === ''){
+    mensageParcela.current.innerText = 'Preencha o numero de parcelas'
+  }else if(entrada === '') {
+    mensageEntrada.current.innerText = 'Preencha o valor de entrada'
+  }else if(porcentagem>80){
+    mensagePorcentagemFinanciamento.current.innerText = 'Porcentagem máxima de financiamento atingida abaixe o valor'
+  }else if(banco === 'bradesco' && prazo>420){
+    mensageParcela.current.innerText = 'Numero de parcelas acima do limite pra esse banco'
+  }else if(entrada<valorEntrada){
+    mensageEntrada.current.innerText = `Valor minimo de entrada R$ ${valorEntrada},00`
+  }else if(juros === ''){
+    mensageJuros.current.innerText = 'Preencha a taxa de juros de acordo com o seu banco.'
+  }else if(Number(prazo)<12){
+    mensageParcela.current.innerText = 'Numero de parcelas mininimas é 12.'
+  }else{
+    while(refJuros.current.hasChildNodes()){
+      refJuros.current.removeChild(refJuros.current.firstChild)
+      refParcela.current.removeChild(refParcela.current.firstChild)
+      refAmortizacao.current.removeChild(refAmortizacao.current.firstChild)
+      refSaldo.current.removeChild(refSaldo.current.firstChild)
+      refValorParcela.current.removeChild(refValorParcela.current.firstChild)
+    }
+
+    btnLimpar.current.style.marginTop = '0px'
+    btnLimpar.current.style.marginLeft = '200px'
+    refTable.current.style.display = 'block'
+    mensagePorcentagemFinanciamento.current.innerText = ''
+    mensageParcela.current.innerText = ''
+    mensageEntrada.current.innerText = ''
+    mensageBanco.current.innerText = ''
+    mensageJuros.current.innerText = ''
+    mensageAniversario.current.innerText = ''
+
+  const values = []
+  values.push(Number(financiamento))
+  const parcela0Span = document.createElement('p')
+  parcela0Span.innerText = 'Parcela 0'
+  appendChildOnce(refParcela.current,parcela0Span)
+
+  const parcela0Juros = document.createElement('p')
+  parcela0Juros.innerText = '-'
+  appendChildOnce(refJuros.current,parcela0Juros)
+  
+  const parcela0Amortizacao = document.createElement('p')
+  parcela0Amortizacao.innerText = '-'
+  appendChildOnce(refAmortizacao.current,parcela0Amortizacao)
+
+  const parcela0Valor = document.createElement('p')
+  parcela0Valor.innerText = '-'
+  appendChildOnce(refValorParcela.current,parcela0Valor)
+
+
+  const parcela0Saldo = document.createElement('p')
+  parcela0Saldo.innerText = 'R$ 000'
+  appendChildOnce(refSaldo.current,parcela0Saldo)
+  const amort = Number(financiamento) / Number(prazo)
+  const parc = (Number(imovel) - Number(entrada)) / prazo
+  for(let i = 1; i<=Number(prazo); i++){
+    values.push((Number(imovel) - Number(entrada)) / prazo)
+    var simulation = {
+    parcelas:`Parcela ${i}`,
+    valorParcela: parc.toFixed(2),
+    juros: '0,48',
+    financiado: financiamento,
+    amortizacao: amort.toFixed(2),
+    }
+
+    const spanParcela = document.createElement('p')
+    spanParcela.innerText = simulation.parcelas
+    appendChildOnce(refParcela.current,spanParcela)
+
+    const spanJuros = document.createElement('p')
+    spanJuros.innerText = simulation.juros
+    appendChildOnce(refJuros.current,spanJuros)
+    
+    const spanAmortizacao = document.createElement('p')
+    spanAmortizacao.innerText = simulation.amortizacao
+    appendChildOnce(refAmortizacao.current,spanAmortizacao)
+
+    const spanValorParcela = document.createElement('p')
+    spanValorParcela.innerText = simulation.valorParcela
+    appendChildOnce(refValorParcela.current,spanValorParcela)
+    simulations.push(simulation)
+}
+    const result = values.reduce((acc,cur)=>{
+    const spanSaldo = document.createElement('p')
+    spanSaldo.innerText = `R$ ${acc.toFixed(2)} - R$ ${cur.toFixed(2)} = R$ ${(acc-cur).toFixed(2)}`
+    appendChildOnce(refSaldo.current,spanSaldo)
+    return acc-cur
+    })
+
+    setImovel('')
+    setFinanciamento('')
+    setEntrada('')
+    setPrazo('')
+    setBanco('Selecione um banco')
+    setJuros('')
   }
-      refTable.current.style.display = 'none'
-      btnRef.current.disabled = false
+  
   }
 
   function appendChildOnce(parentElement, childElement){
@@ -110,23 +262,48 @@ function maxPrazos(ev){
     }
   }
 
- 
+  function limparCampos(){
+    setImovel('')
+    setFinanciamento('')
+    setEntrada('')
+    setPrazo('')
+    setBanco('Selecione um banco')
+    setJuros('')
+  }
 
   function createSimulation(ev){
-  
     ev.preventDefault()
-    const porcentagem = (financiamento/imovel) * 100 
-    if(financiamento === '' && imovel === ''){
+    const nascimento = new Date(aniversario).getFullYear()
+    const anoAtual = new Date().getFullYear()
+    
+    const valorEntrada = (imovel*20) /100
+    const porcentagem = (financiamento/imovel) * 100
+    if(banco === 'Selecione um banco'){
+      mensageBanco.current.innerText = 'Selecione um banco valido para dar continuidade'
+      mensageJuros.current.innerText = 'Preencha a taxa de juros'
+    } 
+    else if(aniversario === ''){
+      mensageAniversario.current.innerText = 'Preencha com sua data de nascimento para dar continuidade.'
+    }else if(anoAtual-nascimento>80){
+      mensageAniversario.current.innerText = 'Idade acima do permitido para financiamento.'
+    }else if(anoAtual-nascimento<18){
+      mensageAniversario.current.innerText = 'Idade abaixo do permitido para financiamento.'
+    }
+    else if(financiamento === '' && imovel === ''){
       mensage.current.innerText = 'Preencha o valor do imovel'
       mensagePorcentagemFinanciamento.current.innerText = 'Preencha o valor do financiamento'
-      mensageParcela.current.innerText = 'Preencha o numero de parcelas'
       mensageEntrada.current.innerText = 'Preencha o valor de entrada'
+      mensageBanco.current.innerText = ''
+      mensageJuros.current.innerText = ''
+      mensageAniversario.current.innerText = ''
     }else if(financiamento === 0 && imovel === 0){
       mensageEntrada.current.innerText = 'Preencha o valor de entrada'
       mensage.current.innerText = 'Preencha o valor do imovel'
-      mensageParcela.current.innerText = 'Preencha o numero de parcelas'
       mensagePorcentagemFinanciamento.current.innerText = 'Preencha o valor do financiamento'
       mensageEntrada.current.innerText = 'Preencha o valor de entrada'
+      mensageBanco.current.innerText = ''
+      mensageJuros.current.innerText = ''
+      mensageAniversario.current.innerText = ''
     }else if(imovel === 0){
       mensage.current.innerText = 'Preencha o valor do imovel'
     }else if(financiamento === 0){
@@ -135,20 +312,26 @@ function maxPrazos(ev){
       mensageParcela.current.innerText = 'Preencha o numero de parcelas'
     }else if(entrada === '') {
       mensageEntrada.current.innerText = 'Preencha o valor de entrada'
-    }
-      else if(porcentagem>80){
+    }else if(porcentagem>80){
       mensagePorcentagemFinanciamento.current.innerText = 'Porcentagem máxima de financiamento atingida abaixe o valor'
     }else if(banco === 'bradesco' && prazo>420){
       mensageParcela.current.innerText = 'Numero de parcelas acima do limite pra esse banco'
+    }else if(entrada<valorEntrada){
+      mensageEntrada.current.innerText = `Valor minimo de entrada R$ ${valorEntrada},00`
+    }else if(juros === ''){
+      mensageJuros.current.innerText = 'Preencha a taxa de juros de acordo com o seu banco.'
+    }else if(Number(prazo)<12){
+      mensageParcela.current.innerText = 'Numero de parcelas mininimas é 12.'
     }else{
-      click++
-      if(click %2 === 1){
-      btnRef.current.disabled = true
-      } 
+      btnLimpar.current.style.marginTop = '0px'
+      btnLimpar.current.style.marginLeft = '200px'
       refTable.current.style.display = 'block'
       mensagePorcentagemFinanciamento.current.innerText = ''
       mensageParcela.current.innerText = ''
       mensageEntrada.current.innerText = ''
+      mensageBanco.current.innerText = ''
+      mensageAniversario.current.innerText = ''
+      mensageJuros.current.innerText = ''
       const values = []
       values.push(Number(financiamento))
       const parcela0Span = document.createElement('p')
@@ -206,11 +389,15 @@ function maxPrazos(ev){
         appendChildOnce(refSaldo.current,spanSaldo)
         return acc-cur
         })
+
         setImovel('')
         setFinanciamento('')
         setEntrada('')
         setPrazo('')
         setBanco('Selecione um banco')
+        setJuros('')
+        btnRef.current.style.display='none'
+        btnAtualizar.current.style.display = 'block'
     } 
     
   }
@@ -243,13 +430,27 @@ function maxPrazos(ev){
       <form 
       className={style.form}
       onSubmit={(ev)=>createSimulation(ev)}
-    
       >
+          <div>
+          <label htmlFor="banco">Banco</label>
+          <span className={style.errorSpan} ref={mensageBanco}></span>
+          <select
+          value={banco}
+          onChange={(ev)=> bancoPrazo(ev)}
+          >
+            <option value="">Selecione um banco</option>
+            <option value="bradesco">Bradesco</option>
+            <option value="santander">Santander</option>
+            <option value="itau">Itau</option>
+          </select>
+        </div>
+
         <label htmlFor="aniversario">Data de nascimento</label>
+        <span className={style.errorSpan} ref={mensageAniversario}></span>
         <input 
         type="date" 
         value={aniversario}
-        onChange={(ev)=>setAniversario(ev.currentTarget.value)}        
+        onChange={(ev)=>checkIdade(ev)}        
         />
         <div>
           <label htmlFor="valorImovel">Valor do imovel</label>
@@ -285,18 +486,7 @@ function maxPrazos(ev){
           />
         </div>
 
-        <div>
-          <label htmlFor="banco">Banco</label>
-          <select
-          value={banco}
-          onChange={(ev)=> bancoPrazo(ev)}
-          >
-            <option value="">Selecione um banco</option>
-            <option value="bradesco">Bradesco</option>
-            <option value="santander">Santander</option>
-            <option value="itau">Itau</option>
-          </select>
-        </div>
+    
         <div>
           <label htmlFor="prazoFinanciamento">Prazo financiamento</label>
           <span className={style.errorSpan} ref={mensageParcela}></span>
@@ -309,6 +499,7 @@ function maxPrazos(ev){
           />
         </div>
         <label htmlFor="juros">Taxa juros</label>
+        <span className={style.errorSpan} ref={mensageJuros}></span>
         <input 
         type="text" 
         name="juros"
@@ -317,9 +508,25 @@ function maxPrazos(ev){
         />
         <button
         ref={btnRef}
+        className={style.btn1}
         >Simular</button>
        
       </form>
+      <button
+        onClick={limparCampos}
+        className={style.btn2}
+        ref={btnLimpar}
+        >
+          Limpar
+        </button>
+
+        <button
+        ref={btnAtualizar}
+        className={style.btn3}
+        onClick={clearTable}
+        >
+          Atualizar
+        </button>
   <div 
   className={style.tableContainer}
   ref={refTable}
@@ -351,13 +558,6 @@ function maxPrazos(ev){
     width={30}
     />
     </button> 
-    <button onClick={clearTable} className={style.btnClear}>
-    <Image
-    alt='clearBtn'
-    src={clear}
-    width={30}
-    />
-    </button>
   </div>
     </main>
     </div>
